0fd6c04874f3bd9c2da5df0987cbced2b9ab43d2
Gabriel-J-code <67019722+Gabriel-J-code@users.noreply.github.com>
2020-09-21 16:25:00 -0300

    commit em partes pois no replit não funcionava

--- cupom.py
+++ cupom.py
@@ -12,5 +12,52 @@ cnpj = "42.591.651/0797-34"
 inscricao_estadual = "244.898.500.113"
 
 def imprime_dados_loja():
-    # Implemente aqui
-    return "" 
+	#linha nome_loja
+	if not nome_loja:
+		return"O campo nome da loja é obrigatório"
+	nota = nome_loja + "\n"
+
+	#linha logradouro, numero, e complemento
+	if not logradouro:
+		return"O campo logradouro do endereço é obrigatório"
+	
+	_numero = "s/n" if not numero else str(numero)
+	
+	_complemento = " " + complemento if complemento else ""
+
+	nota += "%s, %s%s\n" %(logradouro,_numero,_complemento)
+
+	# linha bairro, municipio, e estado
+	_bairro = bairro + " - " if bairro else ""
+
+	if not municipio:
+		return"O campo município do endereço é obrigatório"
+
+	if not estado:
+		return"O campo estado do endereço é obrigatório"
+	
+	nota += "%s%s - %s\n" % (_bairro,municipio,estado)
+
+	#linha cep, e telefone
+	_cep = "CEP:" + cep if cep else ""
+	
+	_telefone = "Tel " + telefone if telefone else ""
+	
+	_telefone = " " + _telefone if cep and telefone else _telefone
+
+	nota += "%s%s\n" % (_cep,_telefone)
+
+	#linha observacao
+	nota += observacao + "\n"
+
+	#linha cnpj
+	if not cnpj:
+		return"O campo CNPJ da loja é obrigatório"
+	nota += "CNPJ: %s\n" %(cnpj)
+	
+	#linha inscricao_estadual
+	if not inscricao_estadual:
+		return"O campo inscrição estadual da loja é obrigatório"
+	nota += "IE: %s\n" % (inscricao_estadual)
+
+	return nota
